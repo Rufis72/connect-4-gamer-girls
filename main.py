b9683f09060ddbbd69044da70ae2aa2ff70f9b5e
CubeShifter <desh.kian@gmail.com>
2025-05-10 19:03:09 -0700

    Update main.py
    Gravity and stuff

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
+import random
 
 
 
-player = 1
+player = 1 #Detirmins the
 def switch_player():
     """
     Switches the player from 1 to 2 or from 2 to 1.
@@ -58,13 +59,73 @@ def get_piece_at(column:int,row:int):
     if row < 0 or row > 5:
         raise ValueError("Row must be between 0 and 5.")
     return board[column][row]
+
+def reset_board():
+    """
+    Resets the board to its initial state.
+    """
+    Tokens.update(reset=True)
+    global board
+    board = [[0 for _ in range(6)] for _ in range(7)]
+
+
+
+
+
+
 pygame.init()
 screen = pygame.display.set_mode(flags = pygame.FULLSCREEN)
+#Basic fullscreenvpygame setup
+
 board_png = pygame.image.load("Board.png")
-
-
+board_screen = pygame.Surface((182, 156))
 Red = pygame.image.load("RedToken.png")
 Yellow = pygame.image.load("YellowToken.png")
+#
+
+class Token(pygame.sprite.Sprite):
+    def __init__(self, color: int, platform: int,column: int):
+        super().__init__()
+        if color == 1:
+            self.image = Red
+        elif color == 2:
+            self.image = Yellow
+        else:
+            raise ValueError("Color must be 1 or 2.")
+        self.rect = self.image.get_rect()
+        self.color = color
+        self.position = (column * 26 + 3, 0 )
+        self.velocity = 1
+        self.rect = self.image.get_rect(bottomleft = self.position)
+        self.platform = platform  
+        self.reset = False 
+        
+        
+
+    def update(self, reset: bool = False):
+        #if not( self.reset == reset):
+        #    velocity = 0
+        if reset:
+            self.reset = True
+            self.velocity = 0
+        
+        if self.reset:
+            self.velocity += 0.75
+            self.rect[1] += self.velocity
+            
+            
+            
+            
+        else:
+            self.velocity += 0.75
+            self.rect[1] = min(self.rect[1] + self.velocity, self.platform * 26 + 3)
+        
+        
+            
+       
+        
+        
+Tokens = pygame.sprite.Group()
 
 run = True
 FPS = 30
@@ -83,8 +144,7 @@ else:
 padding = (width - size[0])/2,(height - size[1])/2
 
 board = [[0 for _ in range(6)] for _ in range(7)]
-def reset_board():
-    board = [[0 for _ in range(6)] for _ in range(7)]
+
 
 
 def place_piece(column:int,piece: int):
@@ -100,25 +160,37 @@ def place_piece(column:int,piece: int):
     row = 5
     while board[column][row] != 0 and row > -1:
         row -= 1
+        
+    if row == -1:
+        return
+    
     board[column][row] = piece
+    
+    Tokens.add(Token(piece, row, column))
+    
 
 
 
 while run:
     #Draw the tokens on the board
-    for i in range(7):
-        for j in range(6):
-            if board[i][j] == 1:
-                board_png.blit(Red, (3 + 26 * i,3+ 26 * j))
-            elif board[i][j] == 2:
-                board_png.blit(Yellow, (3 + 26 * i,3+ 26 * j))
-               
     
+    
+               
+    player = random.choice([1, 2])
     
     clock.tick(FPS)
+   
+    screen.fill((255, 255, 255))
+    
+    Tokens.update()
+    board_screen.fill((0, 0, 0, 0))
+    Tokens.draw(board_screen)
+    board_screen.blit(board_png, (0, 0))
+    screen.blit(pygame.transform.scale(board_screen, size), padding)
+    
+    
     pygame.display.flip()
-    screen.fill((0, 0, 0))
-    screen.blit(pygame.transform.scale(board_png, size), padding)
+    
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -140,4 +212,7 @@ while run:
                 place_piece(5, player)
             if event.key == pygame.K_7:
                 place_piece(6, player)
+            if event.key == pygame.K_r:
+                
+                reset_board()
 
